03ff1cd7376606ec6d83f6ce257afd67174d853d
Daehyeon Cho <wheogus6@naver.com>
2026-05-09 17:04:36 +0900

    로그인 기능 분리

--- src/main/java/core/entity/Member.java
+++ src/main/java/core/entity/Member.java
@@ -4,86 +4,41 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-@DynamicInsert
-@DynamicUpdate
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "member")
 public class Member {
 
     @Id
-    @Column(name = "ucode")
-    private String ucode;
+    @Column(name = "member_code")
+    private String memberCode;
 
-    @Column(name = "mobile_no")
-    private String mobileNo;
+    @Column(name = "member_id")
+    private String memberId;
 
-    @Column(name = "reg_date")
-    private LocalDateTime regDate;
+    @Column(name = "member_name")
+    private String memberName;
 
-    @Column(name = "uname")
-    private String uname;
-
-    @Column(name = "funeral_date")
-    private LocalDateTime funeralDate;
-
-    @Column(name = "pt_link_code")
-    private String ptLinkCode;
-
-    @Column(name = "create_user_url")
-    private String createUserUrl;
-
-    @Column(name = "upd_date")
-    private LocalDateTime updDate;
-
-    @Column(name = "memorial_code")
-    private String memorialCode;
-
-    @Column(name = "pet_name")
-    private String petName;
-
-//    @Column(name = "pt_homepage")
-//    private String ptHomepage;
-
-    @Column(name = "funeral_url")
-    private String funeralUrl;
+    @Column(name = "password")
+    private String password;
 
     @Column(name = "status")
     private String status;
 
-    @Column(name = "expDate")
-    private LocalDateTime expDate;
+    @Column(name = "auth_code")
+    private String authCode;
 
-    @Column(name = "limit_date")
-    private Integer limitDate;
+    @Column(name = "reg_date")
+    private LocalDateTime regDate;
 
-    @Column(name = "terms_agree_date")
-    private LocalDateTime termsAgreeDate;
 
-    @Column(name = "video_url")
-    private String videoUrl;
-
-    @Column(name = "memo")
-    private String memo;
-
-    @Column(name = "member_sttus")
-    private String memberSttus;
-
-    @Column(name = "skin_id")
-    private String skinId;
-
-    @Column(name = "terms1")
-    private String terms1;
-
-    @Column(name = "terms2")
-    private String terms2;
 
 }
