50c2555cbc9e2eb589f68f1e3e63c77e8fc41b6d
Daehyeon Cho <wheogus6@naver.com>
2026-05-10 21:55:44 +0900

    이전 파일 제거

--- src/main/java/core/controller/AdminLoginController.java
+++ src/main/java/core/controller/AdminLoginController.java
@@ -1,12 +1,8 @@
 package core.controller;
 
-import core.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
@@ -14,27 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/admin/login")
 public class AdminLoginController {
 
-    @Autowired
-    AdminService adminServie;
+
 
     @GetMapping("/login")
     public String login() {
         return "admin/adminLogin";
     }
 
-    @PostMapping("/loginCheck")
-    @ResponseBody
-    public String loginCheck(String id, String pwd) throws Exception {
-        String result = adminServie.loginCheck(id, pwd);
-        return result;
-    }
 
-    @PostMapping("/logout")
-    @ResponseBody
-    public String logout() {
-        String result = adminServie.logout();
-        return result;
-    }
+
 
 
 
--- src/main/java/core/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package core.repository;
-
-import core.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
